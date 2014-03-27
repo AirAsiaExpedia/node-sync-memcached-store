63820e59566f86a664d6782289a749b83b252208
Jerome Touffe-Blin <jtouffeblin@expedia.com>
2014-03-27 13:27:39 +1100

    Initial commit, a synchronous Fibers-powered memcached store for SyncCache

---
+++ README.md
@@ -0,0 +1,37 @@
+# sync-memcached-store
+
+Synchronous Fibers-powered Memcached store for [sync-cache](https://github.com/jtblin/sync-cache). The store use [syncho](https://github.com/jtblin/syncho)
+to retrieve data "synchronously" from memcached so the code needs to run in a Fiber.
+
+## Usage
+
+    npm install sync-memcached-store --save
+
+```js
+var Sync = require('syncho')
+    , SyncCache = require('sync-cache')
+    , MC = require('sync-memcached-store')
+    , mc = new MC({uri: 'localhost:11211', maxAge: 1000*60*60, ns: 'mynamespace'})
+    , cache = new SyncCache({store: mc, load: mySyncFunction })
+    ;
+
+Sync(function () {
+  console.log(cache.get('some-key'));
+});
+
+function mySyncFunction () {
+  return 'some value';
+}
+```
+
+### Parameters
+
+- `uri` - memcached endpoint string, can be passed as a `uri` property of the options object instead
+- `options` - an options object
+
+### Options
+
+- `uri` - memcached endpoint
+- `ns` - optional namespace
+- `maxAge` - maximum number of milliseconds to keep items, defaults **60000**
+- `timeout` - operation timeout, defaults **1000**
---
+++ sync-memcached-store.js
@@ -0,0 +1,38 @@
+module.exports = MemcachedStore;
+
+var Memcached = require('memcached'), Sync = require('syncho');
+
+function MemcachedStore (options) {
+  if (!(this instanceof MemcachedStore))
+    return new MemcachedStore(options);
+
+  if (typeof options === 'string') options = {uri: options};
+  if (! options || ! options.uri)
+    throw new Error("Must pass a memcached connection string or an option object with at least a `uri` property.");
+
+  this.lifetime = options.maxAge/1000 || 60;
+
+  // TODO: fix timeout connection not firing!!!
+  this.mc = new MemcachedStore.Memcached(options.uri, {namespace: options.ns || '', timeout: options.timeout || 1000});
+}
+
+MemcachedStore.Memcached = Memcached;
+
+MemcachedStore.prototype.set = function (key, value) {
+  return this.mc.set.sync(this.mc, key, value, this.lifetime);
+};
+
+MemcachedStore.prototype.get = function (key) {
+  return this.mc.get.sync(this.mc, key) || void 0;
+};
+
+MemcachedStore.prototype.del = function (key) {
+  return this.mc.del.sync(this.mc, key);
+};
+
+MemcachedStore.prototype.has = function (key) {
+  return true;
+};
+
+MemcachedStore.prototype.peek = MemcachedStore.prototype.get;
+MemcachedStore.prototype.reset = function () {};
